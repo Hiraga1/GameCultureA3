ad2932fbc9362826ca2747e77a433e19b0b1bdb3
Sieunhanom15 <87540541+Romet0123@users.noreply.github.com>
2026-01-02 01:25:33 +0700

    More Character 60% complete

---
+++ GameCulturesA3/Assets/Scripts/Gameplay/TerrainScanner.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:4a615c1cad76e92c478db820ff2bc295a2a3232e65c7ca39dfebc4247dcbd98a
+size 762
--- GameCulturesA3/Assets/Scripts/UI/AttendanceUIController.cs
+++ GameCulturesA3/Assets/Scripts/UI/AttendanceUIController.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:29a04de80a8e08c5683360060c40492778e48a6643ef66dd25c38f8e554480c7
-size 3147
+oid sha256:215caf795de5faedb06eac5f2400922e83f45b4012260bb40e024d66ef93686b
+size 3282
