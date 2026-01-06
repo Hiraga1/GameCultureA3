bf1833596c19af2c780d81ec67c7032e005a4c1c
Romet0123 <nguyenphathuy0123@gmail.com>
2026-01-06 21:45:56 +0700

    Init stories

--- GameCulturesA3/Assets/Scripts/Data/CharacterData.cs
+++ GameCulturesA3/Assets/Scripts/Data/CharacterData.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1d29b4c037bf1aebf7cba8bfd8cb8610f11f1c44cfd97dcde2ed5d4bd6e7fde5
-size 775
+oid sha256:d06a1ef54eb5465febed048d9c6dfb0b86411bc8c81c3cc782171df7a8b5600b
+size 786
